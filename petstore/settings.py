c16e188ceaad43e2c715e460d56c7d195cb23d57
Nikhil I <66690978+ironnicko@users.noreply.github.com>
2022-09-09 20:59:42 +0530

    Update settings.py

--- petstore/settings.py
+++ petstore/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = getenv("SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
